7b5449e322150ab0629b8782dd5fc97b0655c2a6
Khanh Chung <khanh.d.chung@gmail.com>
2025-02-22 23:05:44 -0800

    apply template to Vec2 and Rect

--- Engine/RectI.h
+++ Engine/RectI.h
@@ -1,25 +1,82 @@
 #pragma once
 
-#include "Vei2.h"
+#include "Vec2.h"
 
-class RectI
+template<typename T>
+class RectI_
 {
 public:
-	RectI() = default;
-	RectI( int left_in,int right_in,int top_in,int bottom_in );
-	RectI( const Vei2& topLeft,const Vei2& bottomRight );
-	RectI( const Vei2& topLeft,int width,int height );
-	bool IsOverlappingWith( const RectI& other ) const;
-	bool IsContainedBy( const RectI& other ) const;
-	bool Contains( const Vei2& point ) const;
-	static RectI FromCenter( const Vei2& center,int halfWidth,int halfHeight );
-	RectI GetExpanded( int offset ) const;
-	Vei2 GetCenter() const;
-	int GetWidth() const;
-	int GetHeight() const;
+#include "RectI.h"
+
+	RectI_::RectI_(T left_in, T right_in, T top_in, T bottom_in)
+		:
+		left(left_in),
+		right(right_in),
+		top(top_in),
+		bottom(bottom_in)
+	{
+	}
+
+	RectI_::RectI_(const Vei2& topLeft, const Vei2& bottomRight)
+		:
+		RectI(topLeft.x, bottomRight.x, topLeft.y, bottomRight.y)
+	{
+	}
+
+	RectI_::RectI_(const Vei2& topLeft, T width, T height)
+		:
+		RectI(topLeft, topLeft + Vei2(width, height))
+	{
+	}
+
+	bool RectI_::IsOverlappingWith(const RectI_& other) const
+	{
+		return right > other.left && left < other.right
+			&& bottom > other.top && top < other.bottom;
+	}
+
+	bool RectI_::IsContainedBy(const RectI_& other) const
+	{
+		return left >= other.left && right <= other.right &&
+			top >= other.top && bottom <= other.bottom;
+	}
+
+	bool RectI_::Contains(const Vei2& poT) const
+	{
+		return poT.x >= left && poT.x < right && poT.y >= top && poT.y < bottom;
+	}
+
+	RectI_ RectI_::FromCenter(const Vei2& center, T halfWidth, T halfHeight)
+	{
+		const Vei2 half(halfWidth, halfHeight);
+		return RectI(center - half, center + half);
+	}
+
+	RectI_ RectI_::GetExpanded(T offset) const
+	{
+		return RectI(left - offset, right + offset, top - offset, bottom + offset);
+	}
+
+	Vei2 RectI_::GetCenter() const
+	{
+		return Vei2((left + right) / 2, (top + bottom) / 2);
+	}
+
+	T RectI_::GetWidth() const
+	{
+		return right - left;
+	}
+
+	T RectI_::GetHeight() const
+	{
+		return bottom - top;
+	}
+
 public:
-	int left;
-	int right;
-	int top;
-	int bottom;
+	T left;
+	T right;
+	T top;
+	T bottom;
 };
+
+typedef RectI_<int> RectI;
--- Engine/Vec2.h
+++ Engine/Vec2.h
@@ -1,21 +1,75 @@
 #pragma once
 
-class Vec2
+template<typename T>
+class Vec2_
 {
 public:
-	Vec2() = default;
-	Vec2( float x_in,float y_in );
-	Vec2 operator+( const Vec2& rhs ) const;
-	Vec2& operator+=( const Vec2& rhs );
-	Vec2 operator*( float rhs ) const;
-	Vec2& operator*=( float rhs );
-	Vec2 operator-( const Vec2& rhs ) const;
-	Vec2& operator-=( const Vec2& rhs );
-	float GetLength() const;
-	float GetLengthSq() const;
-	Vec2& Normalize();
-	Vec2 GetNormalized() const;
+	Vec2_::Vec2_(T x_in, T y_in)
+		:
+		x(x_in),
+		y(y_in)
+	{
+	}
+
+	Vec2_ Vec2_::operator+(const Vec2_& rhs) const
+	{
+		return Vec2_(x + rhs.x, y + rhs.y);
+	}
+
+	Vec2_& Vec2_::operator+=(const Vec2_& rhs)
+	{
+		return *this = *this + rhs;
+	}
+
+	Vec2_ Vec2_::operator*(T rhs) const
+	{
+		return Vec2_(x * rhs, y * rhs);
+	}
+
+	Vec2_& Vec2_::operator*=(T rhs)
+	{
+		return *this = *this * rhs;
+	}
+
+	Vec2_ Vec2_::operator-(const Vec2_& rhs) const
+	{
+		return Vec2_(x - rhs.x, y - rhs.y);
+	}
+
+	Vec2_& Vec2_::operator-=(const Vec2_& rhs)
+	{
+		return *this = *this - rhs;
+	}
+
+	T Vec2_::GetLength() const
+	{
+		return std::sqrt(GetLengthSq());
+	}
+
+	T Vec2_::GetLengthSq() const
+	{
+		return x * x + y * y;
+	}
+
+	Vec2_& Vec2_::Normalize()
+	{
+		return *this = GetNormalized();
+	}
+
+	Vec2_ Vec2_::GetNormalized() const
+	{
+		const T len = GetLength();
+		if (len != (T)0)
+		{
+			return *this * (1.0f / len);
+		}
+		return *this;
+	}
+
 public:
-	float x;
-	float y;
+	T x;
+	T y;
 };
+
+typedef Vec2_<float> Vec2;
+typedef Vec2_<int> Vei2;
